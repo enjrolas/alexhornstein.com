a47902cccfc6d62aa76cd9759dd205cba435d7f7
enjrolas <alex@artiswrong.com>
2022-10-22 13:15:31 -0400

    added thumbnail photos and initial mouseover logic

--- js/script.js
+++ js/script.js
@@ -10,5 +10,34 @@ $( document ).ready(function() {
             console.log(bootstrapDetectBreakpoint())        
 	}, 5000);
     }
+
+    var thumbnails=$("img.thumbnail");
+    var atWorkPhotos=$("img.atWork");
+    var atPlayPhotos=$("img.atPlay");
+    var takingPicturesPhotos=$("img.takingPictures");
+
+    $("#atWork").mouseover(function(){
+	var index=0;
+	thumbnails.each(function(){
+	    this.src=atWorkPhotos[index].src;
+	    index++;
+	});
+    });
+    
+    $("#atPlay").mouseover(function(){
+	var index=0;
+	thumbnails.each(function(){
+	    this.src=atPlayPhotos[index].src;
+	    index++;
+	});	
+    });
+
+    $("#takingPictures").mouseover(function(){
+	var index=0;
+	thumbnails.each(function(){
+	    this.src=takingPicturesPhotos[index].src;
+	    index++;
+	});	
+    });
 });
 
